a5872c957a9ec0a201a011c61cb9bbb1bd03dd1e
sunyso434-cyber <sunyso434@gmail.com>
2026-07-28 16:43:39 +0800

    fix: 修复 xgboostPredictionHandler IPC 注册 bug (Task A0)

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ require('./src/main/ipcHandlers/mixDesignOptimizerHandler') // 新增：优化�
 const SystemHandler = require('./src/main/ipcHandlers/systemHandler')
 require('./src/main/ipcHandlers/aiAnalysisHandler')
 require('./src/main/ipcHandlers/salesQuoteHandler') // 销售报价 IPC 处理器
-require('./src/main/ipcHandlers/xgboostPredictionHandler') // XGBoost性能预测 IPC 处理器
+require('./src/main/ipcHandlers/xgboostPredictionHandler').registerHandlers(ipcMain) // XGBoost性能预测 IPC 处理器
 require('./src/main/ipcHandlers/visionHandler') // Task 8：视觉图片上传/列出 IPC 处理器
 require('./src/main/ipcHandlers/agentHandler').registerAgentHandlers() // AI Agent IPC 处理器
 const { registerLlmHandlers } = require('./src/main/ipcHandlers/llmHandler')
